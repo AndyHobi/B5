bf011b7e26a9bf5b4a693c552697330241e9a20e
lucas <lucas@10.80.148.227>
2018-10-31 11:02:10 -0400

    i made it work

--- LewdingLewdington/src/LewdingClass.java
+++ LewdingLewdington/src/LewdingClass.java
@@ -1,4 +1,7 @@
 
 public class LewdingClass {
 	//place to lewd things
+	public static void lewd(String string) {
+		System.out.println(string + " you have been lewded");
+	}
 }
--- LewdingLewdington/src/PlaceToBeLewdedMain.java
+++ LewdingLewdington/src/PlaceToBeLewdedMain.java
@@ -1,8 +1,15 @@
+import java.util.Scanner;
 
 public class PlaceToBeLewdedMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		String string = new String();
+		System.out.println("Who do you want lewded?");
+		string = scan.nextLine();
+		LewdingClass.lewd(string);
+		
 
 	}
 
